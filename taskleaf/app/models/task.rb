aba944ec1508a0ce331423fa222be36cecac4766
Mitsuhito Numa <tnm14893@yahoo.co.jp>
2022-06-24 21:15:07 +0900

    Added original verification code.

--- taskleaf/app/models/task.rb
+++ taskleaf/app/models/task.rb
@@ -2,4 +2,11 @@ class Task < ApplicationRecord
   # validates :name, presence: true
   # validates :name,  length: { maximum: 30 }
   validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+  
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマは含めることができません') if name&.include?(',')
+  end
 end
