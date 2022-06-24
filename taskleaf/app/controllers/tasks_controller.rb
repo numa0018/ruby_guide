decba44cf91d82360bd4afc59e56b2f04112b23d
Mitsuhito Numa <tnm14893@yahoo.co.jp>
2022-06-24 14:33:04 +0900

    Respond to validation errors in controllers &views

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -22,9 +22,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    # viewに渡したいので、task → @task partial先で{task : @task}設定してる
+    @task = Task.new(task_params)
+
+    if task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def destroy
