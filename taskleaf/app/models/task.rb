af3135f0ebefed5f4e09cbdc13ec031e5716eeed
Mitsuhito Numa <tnm14893@yahoo.co.jp>
2022-06-24 14:54:21 +0900

    Combine the same attributes on one line.

--- taskleaf/app/models/task.rb
+++ taskleaf/app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
-  validates :name,  length: { maximum: 30 }
+  # validates :name, presence: true
+  # validates :name,  length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 }
 end
