bccc8b1ba94518205a08edc32282763d5e37aece
Mitsuhito Numa <tnm14893@yahoo.co.jp>
2022-07-06 20:35:48 +0900

    create user page.

--- taskleaf/app/controllers/admin/users_controller.rb
+++ taskleaf/app/controllers/admin/users_controller.rb
@@ -1,7 +1,20 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
 
   def create
     @user = User.new(user_params)
@@ -13,13 +26,20 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  def edit
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザ「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
-  def show
-  end
-
-  def index
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザ「#{@user.name}」を削除しました。"
   end
 
   private
